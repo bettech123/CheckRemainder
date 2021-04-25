cd581d458adde4131e58919eccf233db6502eb52
bettech123 <bethel89ogu@gmail.com>
2021-04-25 18:31:00 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CheckRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int dividend, divisor, remainder;
+
+            Console.WriteLine("Enter dividend");
+            dividend = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter divisor");
+            divisor = int.Parse(Console.ReadLine());
+
+            remainder = dividend % divisor;
+
+            Console.WriteLine("The remainder of " + dividend + " divided by " + divisor + " = " + remainder);
+        }
+    }
+}
